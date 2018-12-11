4166cc2e02e19a0a7bc9b530f75446e91c8f1b37
nobukazu1203 <nobukazu1203@yahoo.co.jp>
2018-12-11 21:21:23 +0900

    Create sw2.js

---
+++ sw2.js
@@ -0,0 +1,41 @@
+//キャッシュするアセットのリスト
+var urlsToCache = [
+   '/',
+   '/index.html',
+   '/pwa.html'
+];
+var CACHE_NAME = "cache-v1";
+
+//install イベントのハンドラ
+self.addEventListener('install', function(event) {
+   event.waitUntil(
+      //キャッシュを開く
+      caches.open(CACHE_NAME)
+         .then(function(cache) {
+            //アセットのリストをキャッシュに登録
+//            return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+            return cache.addAll(urlsToCache);
+   }));
+});
+
+
+self.addEventListener('activate', function(e) {
+  console.log('activate');
+   e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request)
+           .then(function(response) {
+              return response || fetch(e.request);
+    }));
+})
