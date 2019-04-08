a652743b74c7b85d63270bb4a94b50de58cde791
nobukazu1203 <nobukazu1203@yahoo.co.jp>
2019-04-08 14:50:12 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log("ccccc");
-document.write('<p>document.write</p>');
+document.write('<p>document.write 2</p>');
 document.write('<scr' + 'ipt scr="https://nobukazu1203.github.io/test2js"></scr' + 'ipt>');
